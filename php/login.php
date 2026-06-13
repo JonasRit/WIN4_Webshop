7f45ac13df9c07df8beaba4fb62f3c2f0b50c333
Jonas Ritter <Jritter4@stud.hs-offenburg.de>
2026-06-13 23:34:40 +0200

    Refactor login logic to use prepared statements and streamline session handling

--- php/login.php
+++ php/login.php
@@ -1,37 +1,24 @@
 <?php
+    session_start();
+    
+    $username = isset($_POST["username"]) ? $_POST["username"] : "";
+    $password = isset($_POST["password"]) ? $_POST["password"] : "";
 
     $mysqli = new mysqli("localhost", "root", "", "webshop");
-			if ($mysqli->connect_errno) {
-				die("Verbindung fehlgeschlagen: " . $mysqli->connect_error);
-			}
-			$sql = "SELECT * FROM kunde;";
-			$statement = $mysqli->prepare($sql);
-			$statement->execute();
-			$result = $statement->get_result();
+    if ($mysqli->connect_errno) {
+        die("Verbindung fehlgeschlagen: " . $mysqli->connect_error);
+    }
 
-			
-			while($row = $result->fetch_object()) {
-                
+    $sql = "SELECT * FROM kunde WHERE benutzername = ? AND passwort = ?";
+    $statement = $mysqli->prepare($sql);
+    $statement->bind_param('ss', $username, $password);
+    $statement->execute();
+    $result = $statement->get_result();
 
-                if (isset($_POST["username"])) {
-                    $username = $_POST["username"];
-                }
-                if (isset($_POST["password"])) {
-                    $password = $_POST["password"];
-                }
-                if($username == $row->benutzername && $password == $row->passwort) {
-                    session_start();
-                    $_SESSION["logged_in"] = true;
-                    header("location: ../index.php");
-                } else {
-                    $logged_in = false;
-                    header("location: ./LoginGUI.php");
-                }
-						
-			}
- 
-			
-
-
-    
+    if($result->num_rows == 1) {
+        $_SESSION["logged_in"] = true;
+        header("location: ../index.php");
+    } else {
+        header("location: ./LoginGUI.php");
+    }
 ?>
